dd7c5fedcf805049547c9d7f11a224e79270b6ee
ludo1026 <ludovic.chaboud@sogeti.com>
2014-07-05 09:50:14 +0200

    multiline in export

--- main.js
+++ main.js
@@ -2,22 +2,13 @@ var iml = require('./lib/iml');
 var fs = require('fs');
 var path = require('path');
 
-var data = {
-  n1: {n11: "n111", n12: "n112"},
-  n2: null,
-  n3: null
-};
+// Read
+var filename = "data.iml";
+var fileContent = fs.readFileSync(path.join(process.cwd(), filename), 'utf8');
+var data = iml.iml2js(fileContent);
+console.log(data);
 
-var out = iml.js2iml(data);
-console.log(out);
-
-/*
-var filename = "sample.iml";
-
-var content = fs.readFileSync(path.join(process.cwd(), filename), 'utf8');
-console.log(content);
-
-var output = iml.iml2js(content);
-console.log("output:");
-console.log(output);
-*/
+// Write
+var imlContent = iml.js2iml(data);
+var filename = "data2.iml";
+fs.writeFileSync(path.join(process.cwd(), filename), imlContent, 'utf8');
--- mainWrite.js
+++
@@ -1,9 +0,0 @@
-var iml = require('./lib/iml');
-var fs = require('fs');
-var path = require('path');
-
-var data = "Hello";
-
-var imlContent = iml.js2iml(data);
-var filename = "data2.iml";
-fs.writeFileSync(path.join(process.cwd(), filename), imlContent, 'utf8');
